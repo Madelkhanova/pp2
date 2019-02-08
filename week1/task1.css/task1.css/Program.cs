55ef5f79883ceb6e3f9c4526583cc8afd2cd21d6
Madelkhanova <Madelxanova@mail.ru>
2019-02-08 19:35:03 +0600

    task1 lab1

---
+++ week1/task1.css/task1.css/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task1.css
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            List<int> primes = new List<int>();
+
+            string[] numbers = input.Split();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int current = int.Parse(numbers[i]);
+                bool isTrue = true;
+                for(int j = 2; j <= Math.Sqrt(current); j++)
+                {
+                    if (current % j == 0){
+                        isTrue = false;
+                        break;
+                    
+                    }   
+                }
+                if (isTrue && current!=1)
+                {
+                    primes.Add(current);
+                }
+            }
+            Console.WriteLine(primes.Count);
+            for(int i = 0; i < primes.Count; i++)
+            {
+                Console.Write(primes[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
