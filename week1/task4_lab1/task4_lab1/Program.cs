116f82bb46f182d1b225ad2a34594f2f15926af3
Madelkhanova <Madelxanova@mail.ru>
2019-02-12 15:49:53 +0600

    task4

---
+++ week1/task4_lab1/task4_lab1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace task4_lab1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string pattern = "[*]";
+            int n = int.Parse(Console.ReadLine());
+            string[,] elka = new string[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    elka[i, j] = pattern;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(elka[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+    
