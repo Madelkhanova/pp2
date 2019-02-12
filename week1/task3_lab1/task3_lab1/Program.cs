f456f3531125a61d01e2e2179aeaf808cd38b121
Madelkhanova <Madelxanova@mail.ru>
2019-02-12 15:23:48 +0600

    task3

---
+++ week1/task3_lab1/task3_lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace task3_lab1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            string[] numbers = input.Split();
+            int[] doubleArray = new int[2 * n];
+            int cnt = -1;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                cnt++;
+                doubleArray[cnt] = int.Parse(numbers[i]);
+                cnt++;
+                doubleArray[cnt] = int.Parse(numbers[i]);
+            }
+            for (int i = 0; i < doubleArray.Length; i++)
+            {
+                Console.Write(doubleArray[i] + " ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
