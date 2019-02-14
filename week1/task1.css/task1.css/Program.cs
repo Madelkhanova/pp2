ee9b1713c7111c7c3a7def6cd62df4c6fad2b646
Madelkhanova <Madelxanova@mail.ru>
2019-02-14 13:48:54 +0600

    task1_lab1

--- week1/task1.css/task1.css/Program.cs
+++ week1/task1.css/task1.css/Program.cs
@@ -7,32 +7,32 @@ namespace task1.css
     {
         public static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            string input = Console.ReadLine();
-            List<int> primes = new List<int>();
+            int n = int.Parse(Console.ReadLine());//считываем с консоля string и переволим ево в int с помощью int.Parse .
+            string input = Console.ReadLine();//считываем с консоля string 
+            List<int> primes = new List<int>();// создаем динамический массив
 
-            string[] numbers = input.Split();
-            for (int i = 0; i < numbers.Length; i++)
+            string[] numbers = input.Split();//разделяем нашу строку 
+            for (int i = 0; i < numbers.Length; i++)// пробегаемся по всей длине 
             {
-                int current = int.Parse(numbers[i]);
-                bool isTrue = true;
-                for(int j = 2; j <= Math.Sqrt(current); j++)
+                int current = int.Parse(numbers[i]);//разделенный числа переводим в int
+                bool isTrue = true; // предпологаем что наш bool является true
+                for(int j = 2; j <= Math.Sqrt(current); j++)// проверяем числа на prime 
                 {
                     if (current % j == 0){
                         isTrue = false;
-                        break;
+                        break;//  прерываем
                     
                     }   
                 }
-                if (isTrue && current!=1)
+                if (isTrue && current!=1)// если после проверки на prime number наш bool все еще true (значит число prime) и число не равно единице , мы можем закинуть наши числа в динамический массив 
                 {
-                    primes.Add(current);
+                    primes.Add(current);// закидваем числа в наш дианмический массив
                 }
             }
-            Console.WriteLine(primes.Count);
+            Console.WriteLine(primes.Count);// выводим на экран количсетво простых числе в нашем динамическом массиве
             for(int i = 0; i < primes.Count; i++)
             {
-                Console.Write(primes[i] + " ");
+                Console.Write(primes[i] + " ");//выводим сами числа в нашем динамеском массиве
             }
             Console.ReadKey();
         }
